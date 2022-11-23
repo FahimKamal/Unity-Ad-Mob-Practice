a1eb97ef848665b7d6cffb8c7e57a3fe31b2e20b
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2022-11-23 08:40:08 +0600

    Init commit

---
+++ Ad Mob Practice/Assets/Interstitial.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class Interstitial : MonoBehaviour
+{
+    private InterstitialAd interstitial;
+    
+    public void Start()
+    {
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(initStatus => { });
+
+        this.RequestInterstitial();
+
+        if (interstitial.IsLoaded())
+        {
+            interstitial.Show();
+        }
+    }
+
+    private void RequestInterstitial()
+    {
+        #if UNITY_ANDROID
+                string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+        #elif UNITY_IPHONE
+                string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+        #else
+                string adUnitId = "unexpected_platform";
+        #endif
+
+        // Initialize an InterstitialAd.
+        this.interstitial = new InterstitialAd(adUnitId);
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        this.interstitial.LoadAd(request);
+    }
+}
---
+++ Ad Mob Practice/Assets/Rewarded.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using GoogleMobileAds.Api;
+using UnityEngine;
+
+public class Rewarded : MonoBehaviour
+{
+    private RewardedAd rewardedAd;
+
+    private void Start()
+    {
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(initStatus => { });
+        RequestRewardedAd();
+
+        if (rewardedAd.IsLoaded())
+        {
+            rewardedAd.Show();
+        }
+    }
+
+    public void RequestRewardedAd()
+    {
+        string adUnitId;
+        #if UNITY_ANDROID
+            adUnitId = "ca-app-pub-3940256099942544/5224354917";
+        #elif UNITY_IPHONE
+            adUnitId = "ca-app-pub-3940256099942544/1712485313";
+        #else
+            adUnitId = "unexpected_platform";
+        #endif
+
+        this.rewardedAd = new RewardedAd(adUnitId);
+
+        // Called when an ad request has successfully loaded.
+        this.rewardedAd.OnAdLoaded += HandleRewardedAdLoaded;
+        // Called when an ad request failed to load.
+        this.rewardedAd.OnAdFailedToLoad += HandleRewardedAdFailedToLoad;
+        // Called when an ad is shown.
+        this.rewardedAd.OnAdOpening += HandleRewardedAdOpening;
+        // Called when an ad request failed to show.
+        this.rewardedAd.OnAdFailedToShow += HandleRewardedAdFailedToShow;
+        // Called when the user should be rewarded for interacting with the ad.
+        this.rewardedAd.OnUserEarnedReward += HandleUserEarnedReward;
+        // Called when the ad is closed.
+        this.rewardedAd.OnAdClosed += HandleRewardedAdClosed;
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the rewarded ad with the request.
+        this.rewardedAd.LoadAd(request);
+    }
+
+    public void HandleRewardedAdLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardedAdLoaded event received");
+    }
+
+    public void HandleRewardedAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print(
+            "HandleRewardedAdFailedToLoad event received with message: "
+                             + args.LoadAdError);
+    }
+
+    public void HandleRewardedAdOpening(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardedAdOpening event received");
+    }
+
+    public void HandleRewardedAdFailedToShow(object sender, AdErrorEventArgs args)
+    {
+        MonoBehaviour.print(
+            "HandleRewardedAdFailedToShow event received with message: "
+                             + args.Message);
+    }
+
+    public void HandleRewardedAdClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardedAdClosed event received");
+    }
+
+    public void HandleUserEarnedReward(object sender, Reward args)
+    {
+        Debug.Log("Player has been rewarded");
+    }
+}
